b3b8c3822fdbb77fe41138492d404322515f0fd0
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2017-06-17 15:00:08 -0400

    - A new strategy for handling state verification.  Each state will specify the tray that it expects. - XCAssertEqual is expose to the testing surface instead of buried in a code abstraction - verification must be triggered manually.  this is to prevent magic.  verification is done with a block based api so that the check will look distinct when looking at code at a glance

--- OmnitureVerificationUITests/OmnitureRoute.swift
+++ OmnitureVerificationUITests/OmnitureRoute.swift
@@ -25,6 +25,17 @@ extension OpenSettingsProtocol {
     }
 }
 
+class Tray {var description = ""}
+
+extension OmnitureState {
+    @discardableResult
+    func verify(block: (_ r: String, _ e: String) -> ()) -> Self {
+        let (r, e) = stateVerification()
+        block(r, e)
+        return self
+    }
+}
+
 class OmnitureState {
     let xctestcase: XCTestCase
     let beacon: (mmodule: String, mlink: String)
@@ -34,25 +45,62 @@ class OmnitureState {
     {
         self.xctestcase = xctestcase
         self.beacon = (mmodule, mlink)
-        
-        stateVerification() // verify state
     }
     
     /**
-     This method is called upon state creation.  do not call this directly
+     compares the result to the expected string.  array-ifies the tray description.  anything that is deemed not a match will be added to the result and expected string.  a perfect match would mean result and expeted are empty strings
      */
-    func stateVerification() {
+    final func stateVerification() -> (result: String, expected: String) {
         //<-- verify that the current state is right aka pagename
+        
+        //<-- convert result from tray label into a sorted array.
+        _ = arrayify("app tray label")
+        //<-- convert expected tray object label into a sorted array
+        _ = arrayify(expectedTray.description)
+        
+        //<-- each pair that do not match will be added to the result/expected string and then returned
+        //<-- the xctestequal can compare these strings
+        var _r = ""
+        var _e = ""
+        
+        for (r, e) in zip([String](), [String]()) {
+            if r != e { _r += r + ", "; _e += e + ", " }
+        }
+        // convert tray into a sorted array
+        
+        
+        
+        // for each element that do not match add it to the not matching set.  fail it.
+        
+        return (_r, _e)
     }
-    func signOut_resetEnvironment_deleteContentFromUser(){}
+    
+    private func arrayify(_ str: String) -> [String] {
+        // this represents the tray value
+//        var str = [
+//            "name: bob"
+//            , "count: m"
+//            ].map({ "\"" + $0 + "\"" }).sorted().joined(separator: ", ")
+        
+        // convert string to data
+        let d = "[\(str)]".data(using: .utf8)!
+        // change data to array
+        let arr = (try? JSONSerialization.jsonObject(with: d, options: [])) as? [String]
+        return arr?.sorted() ?? []
+    }
+    
+    open var expectedTray: Tray { return Tray() }
+    
+    internal func signOut_resetEnvironment_deleteContentFromUser(){}
 }
 
 ////////////
 
 class HomeState: OmnitureState, OpenSettingsProtocol {
  
-    override func stateVerification() {
+    override var expectedTray: Tray {
         //<-- verify home-screen
+        return Tray()
     }
     
     func tapMyMeds() {
@@ -64,8 +112,9 @@ class HomeState: OmnitureState, OpenSettingsProtocol {
 }
 
 class SettingsState: OmnitureState {
-    override func stateVerification() {
+    override var expectedTray: Tray {
         //<-- verify settings screen
+        return Tray()
     }
     func tapRow(accessibilityId: String) {/*...*/}
     
@@ -81,8 +130,9 @@ class SettingsState: OmnitureState {
 /////////////
 
 class MyMedsState: OmnitureState {
-    override func stateVerification() {
+    override var expectedTray: Tray {
         //<-- verify MyMeds page name with beacon attributes
+        return Tray()
     }
     func goBackToHome() {
         //<-- if left bar button item is OpenSettings then go home via settings manipulation
@@ -94,8 +144,9 @@ class MyMedsState: OmnitureState {
 /////////////
 
 class LoginPopUpState: OmnitureState {
-    override func stateVerification() {
+    override var expectedTray: Tray {
         //<-- verify module
+        return Tray()
     }
     func tapCancel() {
         //<-- tap cancel and verify module and link
@@ -116,8 +167,10 @@ class LoginPopUpState: OmnitureState {
 class LoginScreenState: OmnitureState {
     var username: String { return "username" }
     var password: String { return "password" }
-    override func stateVerification() {
+    
+    override var expectedTray: Tray {
         //<-- verify login page name
+        return Tray()
     }
     func enterUserCredentials() {
         //<-- enter username and password
@@ -132,8 +185,9 @@ class LoginScreenState: OmnitureState {
 }
 
 class RegScreenState: LoginScreenState {
-    override func stateVerification() {
+    override var expectedTray: Tray {
         //<-- verify registration page name.  beacon may indicate it came from login screen or from login pop up
+        return Tray()
     }
     override func enterUserCredentials() {
         super.enterUserCredentials()
--- OmnitureVerificationUITests/OmnitureVerificationUITests.swift
+++ OmnitureVerificationUITests/OmnitureVerificationUITests.swift
@@ -8,6 +8,8 @@
 
 import XCTest
 
+
+
 class OmnitureVerificationUITests: XCTestCase {
         
     override func setUp() {
@@ -23,6 +25,8 @@ class OmnitureVerificationUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         
         OmnitureState.init(xctestcase: self).signOut_resetEnvironment_deleteContentFromUser()
+        let (r, e) = HomeState(xctestcase: self).stateVerification()
+        XCTAssertEqual(r, e)
     }
     
     override func tearDown() {
@@ -31,46 +35,98 @@ class OmnitureVerificationUITests: XCTestCase {
         super.tearDown()
     }
     
-    func test_goToMyMeds_via_HomeMyMeds_usingLogin() {
-        let hs = HomeState(xctestcase: self)
+    func test_example() {
+        // This style provides a nice way to compartmentalize some parts
+        HomeState(xctestcase: self).verify { (r, e) in
+            XCTAssertEqual(r, e)
+        }.tapMyMeds()
+    }
+    
+    func test_goToMyMeds_via_HomeMyMeds_usingLogin2() {
+        let hs = HomeState(xctestcase: self).verify { (r, e) in
+            XCTAssertEqual(r, e)
+        }
         hs.tapMyMeds()
-        let lp = LoginPopUpState(xctestcase: self)
+        
+        let lp = LoginPopUpState(xctestcase: self).verify { (r, e) in
+            XCTAssertEqual(r, e)
+        }
         lp.tapCancel()
+        
         hs.tapMyMeds()
         lp.tapSignIn()
         
-        LoginScreenState(xctestcase: self, mmodule: "mymeds", mlink: "sgnin")
-            .tapSignIn()
-        MyMedsState(xctestcase: self, mmodule: "login-success", mlink: "")
-            .goBackToHome()
+        let ls = LoginScreenState(xctestcase: self, mmodule: "mymeds", mlink: "sgnin").verify { (r, e) in
+            XCTAssertEqual(r, e)
+        }
+        ls.tapSignIn()
+        
+        let ms = MyMedsState(xctestcase: self, mmodule: "login-success", mlink: "").verify { (r, e) in
+            XCTAssertEqual(r, e)
+        }
+        ms.goBackToHome()
+        
+        hs.verify { (r, e) in
+            XCTAssertEqual(r, e)
+        }
+        hs.tapMyMeds()
+        
+        let ms1 = MyMedsState(xctestcase: self, mmodule: "mymeds", mlink: "").verify { (r, e) in
+            XCTAssertEqual(r, e)
+        }
+        ms1.goBackToHome()
+    }
+    
+    func test_goToMyMeds_via_HomeMyMeds_usingLogin() {
+        let hs = HomeState(xctestcase: self)
+        do { let (r, e) = hs.stateVerification(); XCTAssertEqual(r, e) }
+        hs.tapMyMeds()
+        
+        let lp = LoginPopUpState(xctestcase: self)
+        do { let (r, e) = lp.stateVerification(); XCTAssertEqual(r, e) }
+        lp.tapCancel()
         
         hs.tapMyMeds()
-        MyMedsState(xctestcase: self, mmodule: "mymeds", mlink: "")
-            .goBackToHome()
+        lp.tapSignIn()
+        
+        let ls = LoginScreenState(xctestcase: self, mmodule: "mymeds", mlink: "sgnin")
+        do { let (r, e) = ls.stateVerification(); XCTAssertEqual(r, e) }
+        ls.tapSignIn()
+        
+        let ms = MyMedsState(xctestcase: self, mmodule: "login-success", mlink: "")
+        do { let (r, e) = ms.stateVerification(); XCTAssertEqual(r, e) }
+        ms.goBackToHome()
+        
+        do { let (r, e) = hs.stateVerification(); XCTAssertEqual(r, e) }
+        hs.tapMyMeds()
+        
+        let ms1 = MyMedsState(xctestcase: self, mmodule: "mymeds", mlink: "")
+        do { let (r, e) = ms1.stateVerification(); XCTAssertEqual(r, e) }
+        ms1.goBackToHome()
     }
 
     func test_goToMyMeds_via_SettingsMyMeds_usingLogin() {
-        HomeState(xctestcase: self) //<- begin at home
-            .openSettings()
-        let ss = SettingsState(xctestcase: self)
-        ss.tapMyMeds()
-        
-        LoginPopUpState(xctestcase: self)
-            .tapCancel()
-        ss.tapMyMeds()
-        LoginPopUpState(xctestcase: self)
-            .tapSignIn()
-        
-        LoginScreenState(xctestcase: self, mmodule: "mymeds", mlink: "sgnin")
-            .tapSignIn()
-        MyMedsState(xctestcase: self, mmodule: "login-success", mlink: "")
-            .goBackToHome()
-        HomeState(xctestcase: self)
-            .openSettings()
-        SettingsState(xctestcase: self)
-            .tapMyMeds()
-        MyMedsState(xctestcase: self, mmodule: "mymeds", mlink: "")
-            .goBackToHome()
+//        HomeState(xctestcase: self) //<- begin at home
+//            .openSettings()
+//        let ss = SettingsState(xctestcase: self)
+//        ss.tapMyMeds()
+//        
+//        LoginPopUpState(xctestcase: self)
+//            .tapCancel()
+//        ss.tapMyMeds()
+//        LoginPopUpState(xctestcase: self)
+//            .tapSignIn()
+//        
+//        LoginScreenState(xctestcase: self, mmodule: "mymeds", mlink: "sgnin")
+//            .tapSignIn()
+//        MyMedsState(xctestcase: self, mmodule: "login-success", mlink: "")
+//            .goBackToHome()
+//        HomeState(xctestcase: self)
+//            .openSettings()
+//        SettingsState(xctestcase: self)
+//            .tapMyMeds()
+//        MyMedsState(xctestcase: self, mmodule: "mymeds", mlink: "")
+//            .goBackToHome()
     }
     
 }
